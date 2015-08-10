9c2cd743eb29de0d37f91f26265b9e76f6c55761
Terry Marr & Kendra Ash <student@epicodus.com>
2015-08-10 10:35:40 -0700

    will accept 3 floats as inputs and return whether valid triangle

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,13 +3,17 @@ require('triangle')
 
 describe(Triangle) do
   describe('#is_valid?') do
-    it("accepts three floats as input and returns failure when not triangle.") do
+    it("accepts three integers as input and returns failure when not triangle.") do
       test_triangle = Triangle.new(1,1,3)
       expect(test_triangle.is_valid?()).to(eq(false))
     end
 
-    it("accepts three floats as input and returns true if triangle") do
+    it("accepts three integers as input and returns true if triangle") do
       test_triangle = Triangle.new(3,4,5)
+      expect(test_triangle.is_valid?()).to(eq(true))
+    end
+    it("accepts three floats as input and returns true if triangle") do
+      test_triangle = Triangle.new(1.1,1.1,1.1)
       expect(test_triangle.is_valid?()).to(eq(true))
     end
   end
